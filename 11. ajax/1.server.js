ff41504112a74345fcce6b41879dd827d9620171
liuziran <liuziran@zhongan.io>
2021-02-02 15:08:25 +0800

    test

--- 11. ajax/1.server.js
+++ 11. ajax/1.server.js
@@ -1,57 +1,59 @@
-const path = require('path')
-const fs = require('fs').promises
-const http = require('http')
-const url = require('url')
+const path = require('path');
+const fs = require('fs').promises;
+const http = require('http');
+const url = require('url');
 const chalk = require('chalk');
 
-const {
-  createReadStream,
-} = require('fs')
+const { createReadStream } = require('fs');
 
-http.createServer(async (req ,res) => {
-  if(req.url.match(/favicon\.ico/)) return res.end();
-  let { pathname } = url.parse(req.url)
-  let absPath = path.join(__dirname, pathname)
+http
+  .createServer(async (req, res) => {
+    if (req.url.match(/favicon\.ico/)) return res.end();
+    let { pathname } = url.parse(req.url);
+    let absPath = path.join(__dirname, pathname);
 
-  try {
-    let stats = await fs.stat(absPath)
-    if(stats.isFile()) {
-      createReadStream(absPath).pipe(res);
-    } else {
-    }
-    
-  } catch(err) {
-    let arr = []
-    req.on('data', (chunk) => {
-      arr.push(chunk)
-    })
-    req.on('end', () => {
-      res.setHeader('Access-Control-Allow-Origin', '*'); // * 无法设置cookie
-      res.setHeader('Access-Control-Allow-Methods', 'POST');
-      res.setHeader('Access-Control-Allow-Headers', 'token, content-type')
-      res.setHeader('Access-Control-Max-Age', '1800'); // 30分钟内不发
-      if(pathname === '/user') {
-        switch (req.method) {
-          case 'GET':
-            res.setHeader('Content-Type', 'application/json; charset=UTF-8')
-            return res.end(JSON.stringify({
-              name: 'olifer',
-              age: 18
-            }))
-          case 'POST':
-            let content = Buffer.concat(arr).toString();
-            let data = require('querystring').parse(content);
-            res.end(JSON.stringify(data))
-          
-          default:
-            break;
-        }
+    try {
+      let stats = await fs.stat(absPath);
+      if (stats.isFile()) {
+        createReadStream(absPath).pipe(res);
       } else {
-        console.log(err)
       }
-    })
-    
-  }
-}).listen(3000, () => {
-  console.log(`serverl is listen: ${chalk.blue('http://localhost:3000/index.html')}`)
-})
+    } catch (err) {
+      let arr = [];
+      req.on('data', (chunk) => {
+        arr.push(chunk);
+      });
+      req.on('end', () => {
+        res.setHeader('Access-Control-Allow-Origin', '*'); // * 无法设置cookie
+        res.setHeader('Access-Control-Allow-Methods', 'POST');
+        res.setHeader('Access-Control-Allow-Headers', 'token, content-type');
+        res.setHeader('Access-Control-Max-Age', '1800'); // 30分钟内不发
+        if (pathname === '/user') {
+          switch (req.method) {
+            case 'GET':
+              res.setHeader('Content-Type', 'application/json; charset=UTF-8');
+              return res.end(
+                JSON.stringify({
+                  name: 'olifer',
+                  age: 18,
+                })
+              );
+            case 'POST':
+              let content = Buffer.concat(arr).toString();
+              let data = require('querystring').parse(content);
+              res.end(JSON.stringify(data));
+
+            default:
+              break;
+          }
+        } else {
+          console.log(err);
+        }
+      });
+    }
+  })
+  .listen(3000, () => {
+    console.log(
+      `serverl is listen: ${chalk.blue('http://localhost:3000/index.html')}`
+    );
+  });
--- 12. session/1.cookie.js
+++ 12. session/1.cookie.js
@@ -1,56 +1,60 @@
-const http = require('http')
-const queryString = require('querystring')
-const crypto = require('crypto')
-const chalk = require('chalk')
+const http = require('http');
+const queryString = require('querystring');
+const crypto = require('crypto');
+const chalk = require('chalk');
 
-http.createServer((req, res) => {
-  req.getCookie = (key) => { //name='123'; age='10'
-    let cookies = req.headers['cookie'];
-    cookies= queryString.parse(cookies, ';')
-    console.log(cookies)
-    return cookies[key] || ''
-  }
+http
+  .createServer((req, res) => {
+    req.getCookie = (key) => {
+      //name='123'; age='10'
+      let cookies = req.headers['cookie'];
+      cookies = queryString.parse(cookies, ';');
+      console.log(cookies);
+      return cookies[key] || '';
+    };
 
-  let cookieArray = []
-  res.setCookie = (key, value, options = {}) => {
-    // res.setHeader('Set-Cookie', 'name=123;domain=.olifer1')
-    let arr = []
-    if(options.domain) {
-      arr.push('domain', options.domain)
+    let cookieArray = [];
+    res.setCookie = (key, value, options = {}) => {
+      // res.setHeader('Set-Cookie', 'name=123;domain=.olifer1')
+      let arr = [];
+      if (options.domain) {
+        arr.push('domain', options.domain);
+      }
+      if (options.maxAge) {
+        arr.push('Max-Age', options.maxAge);
+      }
+      if (options.httpOnly) {
+        arr.push('HttpOnly', options.httpOnly);
+      }
+      if (options.signed) {
+        //  签名
+      }
+      // .....
+      cookieArray.push(`${key}=${value};${arr.join('=')}`);
+      return res.setHeader('Set-Cookie', cookieArray);
+    };
+
+    if (req.url === '/read') {
+      let name = req.getCookie('name');
+      console.log(name);
+      res.end(name);
+    } else if (req.url === '/write') {
+      // 过期时间
+      res.setCookie('name', '123', {
+        // maxAge: 10 * 1000,
+        httpOnly: true,
+        signed: true, // 要不要加签名
+      });
+      // 只有在路径🎧域名为.olifer1 才会加的cookie
+      // res.setCookie('name', '123', { domain: '.olifer1' })
+      // res.setCookie('name', '123', { expires: '-1' })
+      // 只有在路径为/a 才会加的cookie
+      // res.setCookie('name', '123', { path: '/a' })
+      res.end('write');
     }
-    if(options.maxAge) {
-      arr.push('Max-Age', options.maxAge)
-    }
-    if(options.httpOnly) {
-      arr.push('HttpOnly', options.httpOnly)
-    }
-    if(options.signed) { //  签名
-
-    }
-    // .....
-    cookieArray.push(`${key}=${value};${arr.join('=')}`)
-    return res.setHeader('Set-Cookie', cookieArray)
-  }
-
-  if(req.url === '/read') {
-    let name = req.getCookie('name')
-    console.log(name)
-    res.end(name)
-
-  } else if(req.url === '/write') {
-    // 过期时间
-    res.setCookie('name', '123', { 
-      // maxAge: 10 * 1000,
-      httpOnly: true,
-      signed: true // 要不要加签名
-    })
-    // 只有在路径🎧域名为.olifer1 才会加的cookie
-    // res.setCookie('name', '123', { domain: '.olifer1' })
-    // res.setCookie('name', '123', { expires: '-1' })
-    // 只有在路径为/a 才会加的cookie
-    // res.setCookie('name', '123', { path: '/a' }) 
-    res.end('write')
-  }
-}).listen(3000, () => {
-  console.log(`serverl is listen: ${chalk.blue('http://localhost:3000/read')}`)
-})
+  })
+  .listen(3000, () => {
+    console.log(
+      `serverl is listen: ${chalk.blue('http://localhost:3000/read')}`
+    );
+  });
